73cc03443c9013bcc6f10c705700276cbecad8dd
Guigui0812 <guillaume.rohee0812@gmail.com>
2023-01-10 19:44:08 +0100

    Initial commit

---
+++ TP1Traitement_Image_Partie1/TP1Traitement_Image/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TP1Traitement_Image
+{
+    class Program
+    {
+
+
+
+        public static int Calcul(int[] tab)
+        {
+            int result = 0;
+
+            for(int i = 0; i < tab.Length; i++)
+            {
+                result += tab[i];
+            }
+
+            return result;
+        }
+
+
+        static void Main(string[] args)
+        {
+            int[] tab = new int[12];
+            for(int i=0; i < tab.Length; i++)
+            {
+                tab[i] = (i + 1) * (i + 1);
+                Console.WriteLine("Valeur du carré n°" + (i + 1) + " est " + tab[i].ToString());             
+            }
+
+            int res = Calcul(tab);
+
+            Console.WriteLine("Somme des entiers : " + res);
+            Console.ReadKey();
+           
+            Console.ReadLine();
+        }
+    }
+}
